0f22a3381848db542d7342405afd8f436245d9dd
watertreestar <watertreestar@gmail.com>
2021-05-15 23:53:28 +0800

    test: ✅ Add test perf dependency

--- src/test/java/com/github/watertreestar/SystemClockTest.java
+++ src/test/java/com/github/watertreestar/SystemClockTest.java
@@ -1,20 +1,36 @@
 package com.github.watertreestar;
 
+import org.databene.contiperf.PerfTest;
+import org.databene.contiperf.junit.ContiPerfRule;
+import org.junit.Rule;
 import org.junit.Test;
 
 import java.util.ArrayList;
 
+
 public class SystemClockTest {
+    @Rule
+    public ContiPerfRule i = new ContiPerfRule();
 
     @Test
+    @PerfTest(threads = 10, duration = 3500)
     public void test(){
         long now = SystemClock.now();
         System.out.println(now);
-        SystemClock.now();
-        SystemClock.now();
-        SystemClock.now();
-        SystemClock.now();
-        SystemClock.now();
-        SystemClock.now();
+
+        /**
+         * result:
+         * samples: 174575
+         * max:     361
+         * average: 20.401191464986397
+         * median:  0
+         */
+    }
+
+    @Test
+    @PerfTest(threads = 10, duration = 3500)
+    public void testNative(){
+        long time = System.currentTimeMillis();
+        System.out.println(time);
     }
 }
