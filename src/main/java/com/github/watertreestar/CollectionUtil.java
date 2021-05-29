1d2b373df6598c1310b52599c4ce2e4ba5a4187b
watertreestar <watertreestar@gmail.com>
2021-05-29 19:38:25 +0800

    feat: ✨ Util for Collection

---
+++ src/main/java/com/github/watertreestar/CollectionUtil.java
@@ -0,0 +1,25 @@
+package com.github.watertreestar;
+
+import java.util.*;
+
+/**
+ * Provides a serial of util to operate or return collection
+ */
+public class CollectionUtil {
+    public static Collection listOf(Object... pieces) {
+        return Arrays.asList(pieces);
+    }
+
+    /**
+     * Return a list copy
+     *
+     * @param from
+     * @param <E>
+     * @return
+     */
+    public static <E> List<E> copyOf(List<E> from) {
+        List<E> dest = new ArrayList<>();
+        Collections.copy(dest, from);
+        return dest;
+    }
+}
