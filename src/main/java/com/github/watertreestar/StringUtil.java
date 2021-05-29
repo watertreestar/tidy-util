75dc31d6ccb19e73d716647d9a2ca68d84ee487a
watertreestar <watertreestar@gmail.com>
2021-05-29 19:39:25 +0800

    feat: ✨ refactor StringUtil

--- src/main/java/com/github/watertreestar/StringUtil.java
+++ src/main/java/com/github/watertreestar/StringUtil.java
@@ -1,6 +1,9 @@
 package com.github.watertreestar;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 public class StringUtil {
     public static final String DOT_CHAR = ".";
@@ -9,6 +12,7 @@ public class StringUtil {
     public static final String SLASH_CHAR = "/";
     public static final String ESCAPE_CHAR = "\\";
     public static final String DOUBLE_SLASH_CHAR = "//";
+    public static final String EMPTY_STRING = "";
 
 
     public static String packageToPath(String pkgName) {
@@ -49,12 +53,69 @@ public class StringUtil {
         return str;
     }
 
-    public static String extractPathFromJarEntry(String jarURLEntryPath){
-        if(jarURLEntryPath == null) {
+    public static String extractPathFromJarEntry(String jarURLEntryPath) {
+        if (jarURLEntryPath == null) {
             return null;
         }
         int start = jarURLEntryPath.indexOf(COLON_CHAR);
         int end = jarURLEntryPath.lastIndexOf(EXCLAMATION_CHAR);
-        return jarURLEntryPath.substring(start + 1,end);
+        return jarURLEntryPath.substring(start + 1, end);
+    }
+
+    public static boolean isBlank(String str) {
+        if (str == null) return true;
+        if (str.trim().equals(EMPTY_STRING)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean anyBlank(String... pieces) {
+        for (String str : pieces) {
+            if (isBlank(str)) return true;
+        }
+        return false;
+    }
+
+    public static String join(String separator, String[] pieces) {
+        return join(Arrays.asList(pieces), separator);
+    }
+
+    public static String join(String[] pieces, String separator) {
+        return join(Arrays.asList(pieces), separator);
+    }
+
+    public static String join(String separator, List<String> pieces) {
+        return join(pieces, separator);
+    }
+
+    public static String join(List<String> pieces, String separator) {
+        StringBuilder buffer = new StringBuilder();
+        Iterator iter = pieces.iterator();
+
+        while (iter.hasNext()) {
+            buffer.append((String) iter.next());
+            if (iter.hasNext()) {
+                buffer.append(separator);
+            }
+        }
+
+        return buffer.toString();
+    }
+
+    public static boolean isEqual(String a, String b) {
+        if (a == null) {
+            return b == null;
+        }
+        return a.equals(b);
+    }
+
+    public static String repeat(char ch, int count) {
+        StringBuilder buffer = new StringBuilder();
+
+        for (int i = 0; i < count; ++i)
+            buffer.append(ch);
+
+        return buffer.toString();
     }
 }
