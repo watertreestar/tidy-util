9e6da7a33e64b2b5fb5dcd402173b6d67826606c
watertreestar <watertreestar@gmail.com>
2021-05-30 22:57:04 +0800

    feat: ✨ util for encryption

---
+++ src/main/java/com/github/watertreestar/CryptoUtil.java
@@ -0,0 +1,16 @@
+package com.github.watertreestar;
+
+/**
+ * Provides AES, encryption
+ */
+public class CryptoUtil {
+    public static String decryptAES(String encryptedSeq, byte[] secret) {
+        // todo
+        return "";
+    }
+
+    public static String encryptAES(String encryptedSeq, byte[] secret) {
+        // todo
+        return "";
+    }
+}
